5cef42af768ff39266df8c4caf4db1571e54a446
Thomas Verwoerd <thomas@keystone-strategic.com>
2022-12-25 06:25:31 +0100

    solution day 25

---
+++ day25/src/main/kotlin/day25/part1/solution.kt
@@ -0,0 +1,38 @@
+package day25.part1
+
+import java.io.BufferedReader
+import kotlin.math.pow
+
+/**
+ * @author verwoerd
+ * @since 25/12/22
+ */
+fun day25Part1(input: BufferedReader): Any {
+  val sum = input.lineSequence()
+    .map { line ->
+      val length = line.length - 1
+      line.foldRightIndexed(0L) { index, c, acc ->
+        acc + 5.0.pow(length - index).toLong() * when (c) {
+          '=' -> -2
+          '-' -> -1
+          '0' -> 0
+          '1' -> 1
+          '2' -> 2
+          else -> error("unknown digit $c")
+        }
+      }
+    }.sum()
+  return rewrite(sum)
+
+}
+
+
+fun rewrite(num: Long): String = when {
+  num == 0L -> ""
+  num % 5 == 0L -> rewrite(num / 5) + "0"
+  num % 5 == 1L -> rewrite(num / 5) + "1"
+  num % 5 == 2L -> rewrite(num / 5) + "2"
+  num % 5 == 3L -> rewrite((num + 2) / 5) + "="
+  num % 5 == 4L -> rewrite((num + 1) / 5) + "-"
+  else -> error("no valid representation")
+}
---
+++ day25/src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+import day25.part1.day25Part1
+
+/**
+ * @author verwoerd
+ * @since 25/12/22
+ */
+fun main() {
+  withResourceFile(::day25Part1)
+}
